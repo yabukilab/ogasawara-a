3a259b888143ffad06a6d0f0dd34df3d94ff8fd3
jurippe <s2242011du@s.chibakoudai.jp>
2024-06-14 17:07:30 +0900

    juritest1

---
+++ htdocs/new.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>新規会員登録ページ</title>
+    <link rel="stylesheet"  href="style1.css">
+</head>
+
+<body>
+    <div class="A">
+        <div class="B">
+            <p class="CIT">CIT sports</p>
+            <p class="signup">新規会員登録</p>
+            <p class="a">以下の内容でよろしければ登録ボタンを押してください</p>
+
+            <p class="number">学籍番号<br>
+            
+            <?php print($_POST['number']."");?>
+
+            <p class="pass">パスワード<br>
+                <input type="password" name="pass">
+            </p>
+            <a input type="new1.php" class="btn btn--a btn--radius">書き直す</a><br><br>
+            <a href="" class="btn btn--blue btn--radius">登録</a>
+            
+        </div>
+    </div>
+</body>
+
+</html>
