9b97d913b0ae086a52fb483b0dd0f4f66b625423
sooooran <s2242095jb@s.chibakoudai.jp>
2024-06-14 15:32:49 +0900

    1.0

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,12 +1,38 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html>
 
 <head>
-  <meta charset='utf-8' />
-  <title>テスト/title>
+    <meta charset="utf-8" />
+    <title>ログインページ</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 
 <body>
+<form action="05authenticate.php" method="POST">
+    <div class="A">
+        <div class="B">
+            <p class="CIT">CIT sports</p>
+            
+            <class="logina">ログイン</p>
+           
+            <p class="number">学籍番号<br><input type="text" name="number">
+            <?php if (isset($_GET['error']) && $_GET['error'] == 'student_number'): ?>
+            <p style="color: red;">学籍番号が存在しません</p>
+            <?php endif; ?>
+            
+            <p class="pass">パスワード<br><input type="password" name="pass"><br>
+            <?php if (isset($_GET['error']) && $_GET['error'] == 'password'): ?>
+            <p style="color: red;">パスワードが正しくありません</p>
+            <?php endif; ?>
+
+            <nav>
+                <div class="link-container"><a href="decide.php">ログイン</a></div>
+            </nav>
+            
+            <p class="signup">新規会員登録は<a href="" class="login">こちら</a></p>
+        </div>
+    </div>
+</form>
 </body>
 
 </html>
