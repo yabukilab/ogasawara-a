fb5c02fa3e660f5df1572c456987d6ffd77bcd8c
jurippe <s2242011du@s.chibakoudai.jp>
2024-06-20 16:37:49 +0900

    1

---
+++ htdocs/01signup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>新規会員登録ページ</title>
+    <link rel="stylesheet"  href="style.css">
+</head>
+
+<body>
+    <div class="A">
+        <div class="B">
+            <p class="CIT">CIT sports</p>
+            <p class="signup">新規会員登録</p>
+            <p class="number">学籍番号<br>
+
+            <form action="new.php"method="post">
+            <input type="text" name="number" size="20">
+            </b>
+            <p class="pass">パスワード<br>
+            <input type="text" name="pass" size="20">
+            
+           <nav>
+                <input type="submit" value="確認する">
+           </nav>
+            <p class="s">ログインは<a href="login.php"class="login">こちら</a></p>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
