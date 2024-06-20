358b4b5b7ca876a3fc6d790964d8ab3860cc9b53
sooooran <s2242095jb@s.chibakoudai.jp>
2024-06-20 15:05:43 +0900

    ２

--- htdocs/table.php
+++ htdocs/table.php
@@ -4,5 +4,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>空き状況確認ページ</title>
+
+        <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
      </head>
 </html>
