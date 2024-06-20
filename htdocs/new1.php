68e5203e791343b69c912338bd4adc7553d940dd
jurippe <s2242011du@s.chibakoudai.jp>
2024-06-20 16:41:24 +0900

    1

--- htdocs/new1.php
+++ htdocs/new1.php
@@ -13,15 +13,17 @@
             <p class="CIT">CIT sports</p>
             <p class="signup">新規会員登録</p>
             <p class="a">以下の内容で登録が完了しました</p>
+            <form action="new1.php"method="post">
 
             <p class="number">学籍番号<br>
             <?php print($_POST['number'].""); ?><br>
 
             <p class="pass">パスワード<br>
-                <input type="password" name="pass">
+            <?php print($_POST['pass'].""); ?>
+
             </p>
             <p class="s">ログインは<a href=""class="login">こちら</a></p>
-            
+            </form>
         </div>
     </div>
 </body>
