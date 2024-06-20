5f8ca1bfa5915256a43e9d1504ec7993ca18e888
jurippe <s2242011du@s.chibakoudai.jp>
2024-06-20 14:44:37 +0900

    1

---
+++ htdocs/new2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>新規会員登録ページ</title>
+    <link rel="stylesheet"  href="style.css">
+</head>
+
+<body>
+    <div class="A">
+        <div class="B">
+            <p class="CIT">CIT sports</p>
+            <p class="signup">新規会員登録</p>
+            <p class="number">学籍番号<br>
+
+            <form action="new.php"method="post">
+            <input type="text" name="number" size="20">
+
+            </b>
+            <p class="pass">パスワード<br>
+                <input type="password" name="pass">
+            </p>
+            <nav>
+                <div class="link-container"><a href="new.php">確認する</a></div>
+            </nav>
+            <p class="s">ログインは<a href="login.php"class="login">こちら</a></p>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
