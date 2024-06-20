16c9831b089ce6a3183c3eda19c9d3a0a165d509
sooooran <s2242095jb@s.chibakoudai.jp>
2024-06-20 15:33:54 +0900

    2

--- htdocs/table.php
+++ htdocs/table.php
@@ -6,18 +6,39 @@
         <title>空き状況確認ページ</title>
 
         <style>
-        table {
+          table {
             width: 50%;
             border-collapse: collapse;
-        }
-        th, td {
+          }
+          th, td {
             border: 1px solid #ddd;
             padding: 8px;
             text-align: left;
-        }
-        th {
+          }
+          th {
             background-color:blue;
-        }
-    </style>
+          }
+        </style>
      </head>
+
+     <body>
+     <div class="A">
+        <div class="B">
+            <p class="CIT">CIT sports</p>
+
+            <p class="sport">競技</p>
+
+            <p class="time">時間</p>
+
+            <table>
+    <thead>
+        <tr>
+            <th>名前</th>
+            <th>年齢</th>
+            <th>職業</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+
+     </body>
 </html>
