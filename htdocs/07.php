d5b6d6f7d1e5e523617181442d57e606b691237e
yuuki135 <s2242040mr@s.chibakoudai.jp>
2024-06-21 15:36:49 +0900

    07

---
+++ htdocs/07.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>予約完了</title>
+    <link rel="stylesheet" href="07.css">
+</head>
+
+<body>
+<form action="05authenticate.php" method="POST">
+    <div class="A">
+        <div class="B">
+            <p class="CIT">CIT sports</p>
+            <p class="date">○月×日（△）</p>
+            <p class="sentence">以下の内容で予約しました
+            <p class="date">⚪︎⚪︎⚪︎<br>⚪︎⚪︎：××〜⚪︎⚪︎：××</p>
+            <p class="note">※この画面は窓口で必要になります。
+                <br>この画面のままにするか、<br>この画面をスクリーンショットで<br>保存してください。
+            <nav>
+               
+            </nav>
+            <p><a href="" class="home">ホームへ戻る</a></p>
+        </div>
+    </div>
+</form>
+</body>
+
+</html>
