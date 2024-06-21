150bf161ad6970e2907262fe1429d315a92f1a70
sooooran <s2242095jb@s.chibakoudai.jp>
2024-06-21 14:38:56 +0900

    2

--- htdocs/table.php
+++ htdocs/table.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>空き状況確認</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="table.css">
     <style>
         table {
             width: 50%;
