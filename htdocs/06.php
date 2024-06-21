5d7acf80eaee7626a49d7d90e76e4a7c8509ba86
yuuki135 <s2242040mr@s.chibakoudai.jp>
2024-06-21 15:29:49 +0900

    06

---
+++ htdocs/06.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>ログインページ</title>
+    <link rel="stylesheet" href="06.css">
+</head>
+
+<body>
+<form action="" method="POST">
+    <div class="A">
+        <div class="B">
+            <p class="CIT">CIT sports</p>
+            <p class="date">○月×日（△）</p>
+            <p class="sentence">以下の内容で<br>予約しますか？
+            <p class="date">⚪︎⚪︎⚪︎<br>⚪︎⚪︎：××〜⚪︎⚪︎：××</p>
+            <nav>
+                <input type="submit" value="予約する" class="ellipse">
+                <a href="" class="back-link">戻る</a>
+            </nav>
+        </div>
+    </div>
+</form>
+</body>
+
+</html>
