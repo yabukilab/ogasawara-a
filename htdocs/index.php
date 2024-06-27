1edd7ffefbedd3c5f1b0ccfd35802f981a9bf12f
yuuki135 <s2242040mr@s.chibakoudai.jp>
2024-06-27 18:04:02 +0900

    test

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <div class="center">
-            <h1 class="cit">CIT Sports</h1>
+            <h1>CIT Sports</h1>
             <h2>ログイン</h2>
 
             <form action="" method="post">
