8c9a1b3ced4978fd1fc77da6c3d985987ff3564f
yuuki135 <s2242040mr@s.chibakoudai.jp>
2024-07-05 14:34:39 +0900

    test

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <div class="center">
-            <h1 class="cit">CIT Sports</h1>
+            <h1><img src="食べたい！.png" alt="test" width="300" height="200"></h1>
             <h2>ログイン</h2>
 
             <form action="authenticate.php" method="post">
