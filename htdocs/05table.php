d699d94f2125ee7cd355d20ccf7ddaca6fa999e6
yuuki135 <s2242040mr@s.chibakoudai.jp>
2024-07-06 16:19:07 +0900

    test

--- htdocs/05table.php
+++ htdocs/05table.php
@@ -33,8 +33,7 @@ if (isset($_POST['logout'])) {
         <div class="center">
             <div><img src="CIT_Sports.jpg" alt="test" width="80%" height="80%"></div>
             <div class="title">予約状況</div>
-            <div class="number2">学籍番号</div>
-            <div class="number2"><?php echo htmlspecialchars($student_number); ?></div>
+            <div class="number2">学籍番号：<?php echo htmlspecialchars($student_number); ?></div>
             
             <div>
             <?php
