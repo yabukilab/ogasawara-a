c330084cdb4a68b90a67e0e20f5270e059664f23
korenaga towa <s2342055we@s.chibakoudai.jp>
2025-06-20 17:58:02 +0900

    a

--- htdocs/register_user.php
+++ htdocs/register_user.php
@@ -74,8 +74,9 @@
         $messageType = '';
 
         function logError($msg) {
-            file_put_contents(__DIR__ . '/user_registration_errors.log', date('Y-m-d H:i:s') . ' - ' . $msg . PHP_EOL, FILE_APPEND);
-        }
+            file_put_contents('C:/xampp/htdocs/ogasawara-a/user_registration_errors.log', date('Y-m-d H:i:s') . ' - ' . $msg . PHP_EOL, FILE_APPEND);
+            }
+
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $student_number = $_POST['student_number'] ?? ''; // student_number로 변경
