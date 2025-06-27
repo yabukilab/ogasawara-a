0455081fd1e66c1322ceba220e1a4402cc8e99a6
nagata mina <s2342092kt@s.chibakoudai.jp>
2025-06-27 15:20:44 +0900

    tameshi

--- htdocs/db_test.php
+++ htdocs/db_test.php
@@ -1,11 +1,14 @@
 <?php
-$$dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
-$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
-$dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
-$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
+// データベース接続情報を設定
+$host     = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']    : '127.0.0.1';
+$username = isset($_SERVER['MYSQL_USER'])   ? $_SERVER['MYSQL_USER']   : 'testuser';
+$password = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
+$dbname   = isset($_SERVER['MYSQL_DB'])     ? $_SERVER['MYSQL_DB']     : 'mydb';
 
 try {
+    // 接続開始
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo '✅ データベースに接続できました！';
 } catch (PDOException $e) {
     echo '❌ 接続エラー: ' . $e->getMessage();
