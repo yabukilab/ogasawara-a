c74d6537c00b660fcf2212f0cec55cd6b7168ea9
choiseungwon <s2342701gp@s.chibakoudai.jp>
2025-06-27 15:17:42 +0900

    hennkou

--- htdocs/db_config.php
+++ htdocs/db_config.php
@@ -20,10 +20,27 @@ $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
 try {
   $db = new PDO($dsn, $dbUser, $dbPass);
+
   # プリペアドステートメントのエミュレーションを無効にする．
   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
   # エラー→例外
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  # 結果フェッチのデフォルトモードを連想配列に設定します (カラム名でアクセスしやすくなります)
+  $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
 } catch (PDOException $e) {
   echo "Can't connect to the database: " . h($e->getMessage());
 }
+
+function getTerName($term_num) {
+  swich ($term_num) {
+    case 1:
+      return "前期"; // 1学期(前期)
+    case 2:
+      return "後期"; // 2学期(後期)
+    default:
+      return "不明"; // 知らない学期
+  }
+}
+
+?>
