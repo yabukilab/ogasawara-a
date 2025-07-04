d92ab426c99a8dba992e652c34d8e5efd4282ff6
choiseungwon <s2342701gp@s.chibakoudai.jp>
2025-07-04 20:03:10 +0900

    asc

--- mydb.sql
+++ mydb.sql
@@ -193,6 +193,7 @@ CREATE TABLE `user_timetables` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `timetable_grade` int(1) DEFAULT 1,
+  `timetable_term` varchar(10) DEFAULT '前期',
   `class_id` int(11) NOT NULL,
   `day` varchar(10) NOT NULL,
   `period` int(2) NOT NULL,
@@ -249,4 +250,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-04 19:49:39
+-- Dump completed on 2025-07-04 20:03:04
