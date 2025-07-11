9b8b7ddd671392e2fa658c421d4b7d0221cf0527
Towa Korenaga <s2342055we@s.chibakoudai.jp>
2025-07-11 09:48:24 +0900

    s

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,6 +11,7 @@ $student_number = htmlspecialchars($_SESSION['student_number']);
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>メニュー</title>
     <link rel="stylesheet" href="css/menu.css">
 </head>
