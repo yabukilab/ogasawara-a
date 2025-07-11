b3d3305e37f9703b540cfe635ae822ccfa7aca56
Towa Korenaga <s2342055we@s.chibakoudai.jp>
2025-07-11 09:46:11 +0900

    s

--- htdocs/login.php
+++ htdocs/login.php
@@ -21,6 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン</title>
     <link rel="stylesheet" href="css/login.css">
 </head>
--- htdocs/register.php
+++ htdocs/register.php
@@ -32,6 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新規登録</title>
     <link rel="stylesheet" href="css/register.css">
     <script src="js/register.js" defer></script>
